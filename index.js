71f30bcf366fabd3b633642ef0234fd07f3dda3d
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-24 19:24:48 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+// Removing main div element
+const element = document.getElementById('main');
+element.remove(element);
+
+// Adding h1 as newHeader variable to the body
+const newHeader = document.createElement('h1');
+
+// Setting id attribute to h1
+newHeader.id = 'victory';
+
+// Setting newHeader textContent to 'I am the champion'
+newHeader.textContent = 'Subira Wafubwa is the champion';
+document.append.newHeader;
